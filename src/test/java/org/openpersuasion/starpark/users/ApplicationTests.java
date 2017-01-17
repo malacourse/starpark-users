251156970399599c5eb4fc30a4ab70cb213641a5
David Gordon <davgordo@redhat.com>
2017-01-17 10:33:07 -0500

    configure test as a @SpringBootTest

--- src/test/java/org/openpersuasion/starpark/users/ApplicationTests.java
+++ src/test/java/org/openpersuasion/starpark/users/ApplicationTests.java
@@ -2,13 +2,13 @@ package org.openpersuasion.starpark.users;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@SpringBootTest
 public class ApplicationTests {
 
 	@Test
@@ -16,3 +16,4 @@ public class ApplicationTests {
 	}
 
 }
+
